85fa49b724334c88b222465f095e563f50a833e7
RBZ-code <romainbezolles@hotmail.fr>
2024-02-25 01:31:22 +0100

    fist commit

---
+++ main.js
@@ -0,0 +1,5 @@
+<script>
+    document.getElementById('custom-toggler').addEventListener('click', function() {
+        this.classList.toggle('no-outline') // Ajoute ou retire la classe 'no-outline'
+    });
+</script>
